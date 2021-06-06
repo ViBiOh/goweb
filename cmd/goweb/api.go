a9c989d911ec1a3aa45c947f8d590f3e6dce8442
Vincent Boutour <bob@vibioh.fr>
2021-06-06 15:07:24 +0200

    feat: Disabling gzip compression for prometheus
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/goweb/api.go
+++ cmd/goweb/api.go
@@ -36,7 +36,7 @@ func main() {
 
 	alcotestConfig := alcotest.Flags(fs, "")
 	loggerConfig := logger.Flags(fs, "logger")
-	prometheusConfig := prometheus.Flags(fs, "prometheus")
+	prometheusConfig := prometheus.Flags(fs, "prometheus", flags.NewOverride("Gzip", false))
 	owaspConfig := owasp.Flags(fs, "")
 	corsConfig := cors.Flags(fs, "cors")
 
