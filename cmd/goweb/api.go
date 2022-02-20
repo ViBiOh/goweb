e21609ad37fc7ec42a764e88b18e1deb1f844cea
Vincent Boutour <bob@vibioh.fr>
2022-02-20 14:11:27 +0100

    fix: Adding tracer middleware
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/goweb/api.go
+++ cmd/goweb/api.go
@@ -90,7 +90,7 @@ func main() {
 	})
 
 	go promServer.Start("prometheus", healthApp.End(), prometheusApp.Handler())
-	go appServer.Start("http", healthApp.End(), httputils.Handler(appHandler, healthApp, recoverer.Middleware, prometheusApp.Middleware, owasp.New(owaspConfig).Middleware, cors.New(corsConfig).Middleware))
+	go appServer.Start("http", healthApp.End(), httputils.Handler(appHandler, healthApp, recoverer.Middleware, prometheusApp.Middleware, tracerApp.Middleware, owasp.New(owaspConfig).Middleware, cors.New(corsConfig).Middleware))
 
 	healthApp.WaitForTermination(appServer.Done())
 	server.GracefulWait(appServer.Done(), promServer.Done())
